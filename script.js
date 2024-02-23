d0c48c2c8576c77341022a0d262f9cf198802ddf
Master2233333 <69020456+Master2233333@users.noreply.github.com>
2024-02-23 13:38:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function speakText(person) {
       Schule: ${person.school}.
       Haare: ${person.Haare}.
       Gewicht: ${person.Gewicht}.
-      Größe: ${person.Größe} "Meter"
+      Größe: ${person.Größe}
       Single: ${person.Single}.
       Partner: ${person.Partner}.
       Mutter: ${person.Mutter}.
